2fb5023cb21bb78842c6a4203d1fbf6d812d9409
Rose Briggs <rpbriggs@smith.edu>
2025-06-25 12:36:01 -0400

    IT CAN READ ALBUM ART!!!! RAAAA

--- src/components/SongData.ts
+++ src/components/SongData.ts
@@ -1,26 +1,35 @@
-import { readFile } from 'fs';
-import { IAudioMetadata, parseBlob, selectCover, type IPicture } from 'music-metadata';
-import { useEffect, useState } from 'react';
+import { IAudioMetadata, parseBlob, type IPicture } from 'music-metadata';
+import { useEffect } from 'react';
 
 class SongData {
 	public Title:string = "Loading Title...";
 	public Artist:string = "Loading Artist...";
+	// public TrackNum:number = 0;
 	public AlbumArt:string = "./src/assets/blank_cd.jpg";
 	public FilePath:string;
 	public FileBlob:IAudioMetadata|null = null;
 
 	constructor(filePath : string) {
 		this.FilePath = filePath;
-		// console.log(filePath);
-
 		GetSongData(this);
 	}
 
-	public updateTrackInfo() {
+	public UpdateTrackInfo() {
 		this.Title = this.FileBlob?.common.title? this.FileBlob.common.title : this.Title;
 		this.Artist = this.FileBlob?.common.artist? this.FileBlob.common.artist : this.Artist;
+		// this.TrackNum = this.FileBlob?.common.track? this.FileBlob.common.track : this.TrackNum;
 		console.log(this.Title);
 		
+	}
+
+	public SetAlbumArt(blob:Uint8Array|null) {
+		if(blob != null) {
+			this.AlbumArt = URL.createObjectURL(
+				new Blob([blob as BlobPart], {
+					type: "image/jpeg"
+				})
+            );
+		}
 	}
 }
 
@@ -31,9 +40,9 @@ function GetSongData(songData:SongData){
 				.then(response => response.blob())
 				.then(blob => parseBlob(blob))
 				.then(blob => songData.FileBlob = blob)
-				.then(blob => songData.updateTrackInfo())
-			// const cover = selectCover(common.picture);
-			// setAlbumArt(cover);
+				.then(blob => blob.common.picture? blob.common.picture[0] : null)
+				.then(blob => songData.SetAlbumArt(blob? blob.data : null))
+				.then(blob => songData.UpdateTrackInfo());
 		}
 
 		GetSongDataAsync();
@@ -42,6 +51,5 @@ function GetSongData(songData:SongData){
 
 
 export {
-	SongData,
-	GetSongData
+	SongData
 }
